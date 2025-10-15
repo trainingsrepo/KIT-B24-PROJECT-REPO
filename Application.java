c1a071e6bf1744c921e9e339d067ae1428c9bada
trainingskumar <108652786+trainingsrepo@users.noreply.github.com>
2025-10-15 12:47:04 +0530

    Update Application.java

--- Application.java
+++ Application.java
@@ -27,8 +27,9 @@ class Bank {
             public static void sampleMethod(int a, int b) throws ArithmeticException{
         System.out.println("Hello, this is sample method");
         int c = a/b; //  int c = 4/2 which gets calculated as 2 
-        System.out.println("c:"+c); // c: 2
+       
    }
         #END OF WEBHOOK CODE
     }
+
 
